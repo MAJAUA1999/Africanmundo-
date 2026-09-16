507b0a1fd24054219317e5625ca34ad6170a383b
MAJAUA1999 <pauloluisantoniom@gmail.com>
2026-09-16 12:35:14 +0200

    Refactor filter logic for sports news category

--- app.js
+++ app.js
@@ -1162,30 +1162,26 @@ function renderizarPagina(){
   */
 
   renderizarLista(
-    "
-     "desporto",
-noticias
-.filter(n => {
+  "desporto",
+  noticias
+    .filter(n => {
 
-    const cat =
-      normalizarTexto(
-        n.categoria
+      const cat =
+        normalizarTexto(
+          n.categoria
+        );
+
+      return (
+        cat.includes("desporto") ||
+        cat.includes("esporte") ||
+        cat.includes("sports") ||
+        cat.includes("basquet") ||
+        cat.includes("atlet")
       );
 
-    return (
-      cat.includes("desporto") ||
-      cat.includes("esporte") ||
-      cat.includes("sports") ||
-      cat.includes("basquet") ||
-      cat.includes("atlet")
-    );
-
-  })
-  .slice(0,4)
-
+    })
+    .slice(0,4)
 );
-
-}
 
 /* ==========================================
 ERRO DE NOTÍCIAS
