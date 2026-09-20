9b5a9c3d8e654621c8cc8de3b75c020fc4debaca
MAJAUA1999 <pauloluisantoniom@gmail.com>
2026-09-20 13:49:43 +0200

    Update className for news card element

--- app.js
+++ app.js
@@ -299,7 +299,7 @@ function criarCard(noticia){
 
   const artigo=document.createElement("article");
 
-  artigo.className="news-card compacto";
+  artigo.className="news-card";
 
   artigo.innerHTML=`
 
